c8cfda5cbbe9c67caeac2c26e4838bb6ca96dbcd
le0iscoding <158987418+le0iscoding@users.noreply.github.com>
2024-04-07 12:21:17 -0700

    Update playlist.js

--- commands/playlist.js
+++ commands/playlist.js
@@ -258,7 +258,7 @@ await interaction.editReply({
           url: 'https://discord.gg/FUEHs7RCqz'
         })
         .setDescription(`Hey <@${interaction.member.id}>, your song has been Added successfully! ✨`)
-        .setFooter({ text: 'YouTube - RTX GAMING' })
+        .setFooter({ text: 'YouTube - ExoticLeo' })
         await interaction.reply({
   content: '',
   embeds: [ loadingembed ] 
@@ -484,7 +484,7 @@ await interaction.editReply({
               .setThumbnail(interaction.user.displayAvatarURL({ size: 2048, dynamic: true }))
               .setColor(client.config.embedColor)
               .setDescription('Timeout Use command again!'.replace("{name}", name))
-              .setFooter({ text: 'YouTube - RTX GAMING' })
+              .setFooter({ text: 'Youtube - ExoticLeo' })
             return interaction.editReply({ embeds: [embed], components: [button] }).catch(e => { })
 
           })
@@ -507,7 +507,7 @@ await interaction.editReply({
           .setDescription(`\n${playlist?.playlist?.map(data =>
             `\n**${number++} |** \`${data.name}\` - **${playlist?.musics?.filter(m => m.playlist_name === data.name)?.length || 0}** plays (<t:${Math.floor(data.createdTime / 1000) }:R>)`
           ) }`)
-          .setFooter({ text: 'YouTube - RTX GAMING' })
+          .setFooter({ text: 'YouTube - ExoticLeo' })
         return interaction.reply({ embeds: [embed] }).catch(e => { }) 
 
       }
